ca45637537e8fc8e666d8491f7c0c320f7867b4f
Chau Tran <nartc7789@gmail.com>
2022-01-16 23:37:21 -0600

    fix(redux): make LatestPipe type-safe

--- libs/redux/src/lib/latest/latest.pipe.ts
+++ libs/redux/src/lib/latest/latest.pipe.ts
@@ -1,11 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import type { SelectorResult } from '../types';
 
 @Pipe({
   name: 'latest',
   pure: false,
 })
-export class LatestPipe implements PipeTransform {
-  transform(value: () => any) {
+export class LatestPipe<T> implements PipeTransform {
+  transform(value: SelectorResult<T>): T {
     return value();
   }
 }
